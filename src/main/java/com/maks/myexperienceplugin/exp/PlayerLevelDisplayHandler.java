b12c83b29d521cb2feab2822e019995c07f89182
Maksymilian Stuglik <mastu2115@gmail.com>
2025-08-01 20:09:34 +0200

    Refresh player display after death and rank changes

--- src/main/java/com/maks/myexperienceplugin/exp/PlayerLevelDisplayHandler.java
+++ src/main/java/com/maks/myexperienceplugin/exp/PlayerLevelDisplayHandler.java
@@ -1,21 +1,27 @@
 package com.maks.myexperienceplugin.exp;
 
 import com.maks.myexperienceplugin.MyExperiencePlugin;
+import com.earth2me.essentials.Essentials;
+import com.earth2me.essentials.User;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
 public class PlayerLevelDisplayHandler implements Listener {
 
     private final MyExperiencePlugin plugin;
+    private final Essentials essentials;
 
     public PlayerLevelDisplayHandler(MyExperiencePlugin plugin) {
         this.plugin = plugin;
-        setupScoreboardTeam();
+        this.essentials = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
     }
 
     @EventHandler
@@ -25,6 +31,11 @@ public class PlayerLevelDisplayHandler implements Listener {
 
         // Aktualizacja wszystkich graczy w tabie po dołączeniu nowego gracza
         Bukkit.getScheduler().runTaskLater(plugin, this::updateAllPlayerTabs, 20L);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        removePlayerTeam(event.getPlayer());
     }
 
     public void updatePlayerTab(Player player) {
@@ -37,20 +48,52 @@ public class PlayerLevelDisplayHandler implements Listener {
             return;
         }
 
-        // Pobierz team "level_display"
-        Team team = scoreboard.getTeam("level_display");
+        String teamName = "level_" + player.getName();
+        Team team = scoreboard.getTeam(teamName);
         if (team == null) {
-            plugin.getLogger().warning("Team 'level_display' not found. Creating a new one.");
-            team = scoreboard.registerNewTeam("level_display");
+            team = scoreboard.registerNewTeam(teamName);
             team.setAllowFriendlyFire(true);
             team.setCanSeeFriendlyInvisibles(false);
         }
+        String rankPrefix = "";
+        if (plugin.getLuckPerms() != null) {
+            net.luckperms.api.model.user.User lpUser = plugin.getLuckPerms().getUserManager().getUser(player.getUniqueId());
+            if (lpUser != null) {
+                String lpPrefix = lpUser.getCachedData().getMetaData().getPrefix();
+                if (lpPrefix != null) {
+                    rankPrefix = lpPrefix;
+                }
+            }
+        }
 
-        // Dodaj gracza do teamu
+        team.setPrefix(String.format("§b[ %d ] §r%s", level, rankPrefix));
         team.addEntry(player.getName());
 
-        // Zaktualizuj Tab
-        player.setPlayerListName(String.format("§b[ %d ] §r%s", level, player.getName()));
+        String display = player.getName();
+        if (essentials != null) {
+            User user = essentials.getUser(player);
+            if (user != null) {
+                String nick = user.getNick();
+                if (nick != null && !nick.isEmpty()) {
+                    display = nick;
+                }
+            }
+        }
+        String formatted = String.format("§b[ %d ] §r%s%s", level, rankPrefix, display);
+        player.setPlayerListName(formatted);
+        player.setCustomName(formatted);
+        player.setCustomNameVisible(true);
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        Bukkit.getScheduler().runTaskLater(plugin, () -> updatePlayerTab(event.getPlayer()), 1L);
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        Bukkit.getScheduler().runTaskLater(plugin, () -> updatePlayerTab(player), 1L);
     }
 
     public void updateAllPlayerTabs() {
@@ -59,18 +102,14 @@ public class PlayerLevelDisplayHandler implements Listener {
         }
     }
 
-    private void setupScoreboardTeam() {
+    private void removePlayerTeam(Player player) {
         Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
         if (scoreboard == null) {
-            plugin.getLogger().warning("Scoreboard not available. Team setup skipped.");
             return;
         }
-
-        // Utwórz team "level_display", jeśli nie istnieje
-        if (scoreboard.getTeam("level_display") == null) {
-            Team team = scoreboard.registerNewTeam("level_display");
-            team.setAllowFriendlyFire(true);
-            team.setCanSeeFriendlyInvisibles(false);
+        Team team = scoreboard.getTeam("level_" + player.getName());
+        if (team != null) {
+            team.unregister();
         }
     }
 }
